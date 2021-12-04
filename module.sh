439d9f49b714c149f3aae656c85407f88dd497d4
vudat412 <vudat412@gmail.com>
2021-12-04 19:31:59 +0700

    to be continue...

---
+++ module.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# cài đặt module
+apt-get update -y &&
+    apt-get install vim -y &&
+    fwconsole ma download timeconditions &&
+    fwconsole ma download ucp &&
+    fwconsole ma download setcid &&
+    fwconsole ma download certman &&
+    fwconsole ma download calendar &&
+    fwconsole ma download webrtc &&
+    fwconsole ma download ringgroups &&
+    fwconsole ma download backup &&
+    fwconsole ma download queues &&
+    fwconsole ma download ivr &&
+    fwconsole ma download cel &&
+    fwconsole ma download userman &&
+    fwconsole ma download api &&
+    fwconsole ma download restapi &&
+    fwconsole ma install restapi &&
+    fwconsole ma install userman &&
+    fwconsole ma install api &&
+    fwconsole ma install cel &&
+    fwconsole ma install ivr &&
+    fwconsole ma install queues &&
+    fwconsole ma install backup &&
+    fwconsole ma install ringgroups &&
+    fwconsole ma install calendar &&
+    fwconsole ma install webrtc &&
+    fwconsole ma install certman &&
+    fwconsole ma install ucp &&
+    fwconsole ma install setcid &&
+    fwconsole ma install timeconditions
+# nang cap module
+fwconsole ma upgrade --edge framework api certman firewall sysadmin 
+fwconsole ma downloadinstall core --edge
+fwconsole ma upgradeall
+#etc.
+echo "permitsion key \n"
+chmod 600 /etc/asterisk/keys/api_oauth.key
+chmod 600 /etc/asterisk/keys/api_oauth_public.key
+echo "update packet \n"
+apt-get update -y
+# mở port 
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp -m udp --dport 5060 -j ACCEPT
+iptables -A INPUT -p udp -m udp --dport 10000:65000 -j ACCEPT
+apt-get install ufw -y
+ufw enable
+ufw allow 10000:65000/udp
+ufw allow 5060/udp
+ufw allow 80/tcp
+ufw reload
+fwconsole ma updateall
+sudo ufw reload
---
+++ user.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# tao nguoi dung qua post va lay cookie
+curl -X POST 'http://192.168.1.26:8080/admin/config.php' \
+    -d 'action=setup_admin&username=admin&password1=admin&password2=admin&email=vudat412@gmail.com&system_ident=VoIP+Server&auto_module_updates=enabled&auto_module_security_updates=enabled&unsigned_module_emails=enabled&update_every=saturday&update_period=4to8' \
+    -c /var/www/html/cookieLogin.json 
+    # -c nếu muốn lưu lại cookie user 
+# thiet lap ngon ngu english
+curl -X POST 'http://192.168.1.26:8080/admin/config.php' \
+    -d 'username=admin&password=admin&oobeSoundLang=en&oobeGuiLang=en_US'
+# tạo biến cookie tiện cắt chuỗi luôn :v
+cookie="$(curl -X POST -v --silent http://192.168.1.26:8080/admin/config.php \
+    -d 'username=admin&password=admin&oobeSoundLang=en&oobeGuiLang=en_US' --stderr - | grep expire | grep -oP 'PHPSESSID=([a-zA-Z0-90]+)' | awk -F "=" '{print $2}')"
+# curl từ biến và gán tao key api_application 
+curl -X POST 'http://192.168.1.26:8080/admin/ajax.php?module=api&command=add_application' \
+    --header 'Referer: http://192.168.1.26:8080/admin/config.php?display=api' \
+    --cookie "PHPSESSID=$cookie" \
+    -d 'type=client_credentials&name=API&description=GRAPH+API&website=&redirect=&allowed_scopes=gql&user=' > /var/www/html/token.json
+# cắt chuỗi client_id 
+clientID="$(awk -F ":" '{print $5}' /var/www/html/token.json | cut -d, -f1 | cut -d '"' -f2)"
+echo $clientID
+# cắt chuỗi client_secret
+clientSC="$(awk -F ":" '{print $6}' /var/www/html/token.json | cut -d, -f1 | cut -d '"' -f2)"
+echo $clientSC
+# Tạo Token API GRAPHQL 
+curl -X POST \
+-H "Accept:*/*" \
+-H "Content-Type: application/json" \
+-w "\n" \
+-d '{"client_id":"'"$clientID"'","client_secret":"'"$clientSC"'","grant_type":"'"client_credentials"'","scope":"'"gql"'"}' \
+http://192.168.1.26:8080/admin/api/api/token >> /var/www/html/tokenAPI.json
+# chú ý: ko dc thay đổi nội dung bên trên do fix cứng 1 chiều
